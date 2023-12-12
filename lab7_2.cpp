a47076bae3a8d71f3ca855289ddba0173ab5a03d
Your Name <you@example.com>
2023-12-12 15:26:14 +0700

    lab7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -8,7 +8,7 @@ int main(){
     cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
     cout << "?????: ";
     getline(cin,name);
-    cout << "Fahsai: Wow!!! " << name << "is a really cool name.\n";
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
 
     int studentID;
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
@@ -16,7 +16,7 @@ int main(){
     cin >> studentID;
     int id = studentID/pow(10,7);
     int gear = id-12;
-    cout << "Fahsai: I think you may be GEAR" << gear <<".I have a free movie ticket for you.\n";
+    cout << "Fahsai: I think you may be GEAR " << gear <<". I have a free movie ticket for you.\n";
     cout << "Fahsai: Let's go to the cinema together!!!\n";
 
     string ans;
@@ -29,7 +29,7 @@ int main(){
     cout << "Fahsai: So....which day are you free to go with me?\n";
     cout << name << ": ";
     getline(cin,day);
-    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you./n";
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
     cout << name << ": ";
     cin.ignore();
     getline(cin,ans);
