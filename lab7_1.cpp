5463c50f639ec113a605444eae4fc32ea149a238
Your Name <you@example.com>
2023-12-12 15:48:20 +0700

    lab7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int a  =  20; 
-    int b = 30;
-    int c = 4;
+    double a  =  20; 
+    double b = 30;
+    double c = 4;
     double output;
 
     output = a - b/c;
